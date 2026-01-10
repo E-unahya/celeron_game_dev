3150dc8875b85725fff06e4b9d958a40a6e95299
E-una <65tougames@gmail.com>
2026-01-10 23:15:47 +0900

    Natureのマップを作成、またコヨーテジャンプと移動に慣性を追加

--- 3DModel/Ultimate Platformer Pack - Dec 2021/Character/glTF/player.gd
+++ 3DModel/Ultimate Platformer Pack - Dec 2021/Character/glTF/player.gd
@@ -23,6 +23,9 @@ var rotation_speed : float = 5.0
 
 var target_rotation : float = 0.0
 
+var coyote_time : float = 0.15
+var coyote_time_counter : float = 0.0
+
 # ダメージを受けたときの管理、ハザード編
 enum Hazard {
 	NONE = -1,
@@ -47,9 +50,13 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		coyote_time_counter -= delta
+	else:
+		coyote_time_counter = coyote_time
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and ( is_on_floor() or coyote_time_counter > 0.0):
 		velocity.y = JUMP_VELOCITY
+		coyote_time_counter = 0.0
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
