1a2b0b5ba36d41c617d9c03f54bd87e2db893774
e-unahya <65tougames@gmail.com>
2026-06-24 23:25:21 +0900

    大岩が落ちて転がってくるステージのプロトタイプを実装

--- 3DModel/ohiwa.gd
+++ 3DModel/ohiwa.gd
@@ -1,10 +1,12 @@
 extends RigidBody3D
+# 大岩をRigidBodyにしたの失敗っぽいしArea3DとAnimationPlayerを組み合わせて落ちた専用のアニメーションを再生してそれが終わったら当たり判定が飛ぶ仕組みを実装すればいいんだ
 
 class_name Ohiwa
 @export var forward : Vector3 = Vector3.MODEL_FRONT
 @export var speed : float = 0
 @onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
 @onready var ray_cast_3d: RayCast3D = $RayCast3D
+@onready var shadow_mesh: MeshInstance3D = $ShadowMesh
 
 
 func _ready() -> void:
@@ -14,9 +16,17 @@ func _ready() -> void:
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	if not freeze:
-		add_constant_central_force(forward)
+		add_constant_central_force(forward * 5)
 		if ray_cast_3d.is_colliding():
-			apply_impulse(Vector3.UP * 10)
+			apply_impulse(Vector3.UP * 100)
+
+
+func _physics_process(delta: float) -> void:
+	if !freeze and ray_cast_3d.is_colliding():
+		shadow_mesh.position = position - Vector3.UP * 10 * delta
+		shadow_mesh.global_rotation = Vector3.ZERO
+		shadow_mesh.global_position = ray_cast_3d.target_position
+
 
 func _on_area_3d_body_entered(body:Node3D) -> void:
 	if body is Player:
--- 3DModel/ohiwa_rakka_area.gd
+++ 3DModel/ohiwa_rakka_area.gd
@@ -1,15 +1,36 @@
 extends Area3D
 
-@onready var ohiwa: Ohiwa = $Ohiwa
+@onready var ohiwa : MeshInstance3D = $Ohiwa
+@onready var animation_player :AnimationPlayer= $AnimationPlayer
+@onready var collision_shape_3d: CollisionShape3D = $CollisionShape3D
+@onready var shadow_mesh : MeshInstance3D = $ShadowMesh
 
-@export var forward : Vector3 = Vector3.MODEL_FRONT
-@onready var shadow_mesh: MeshInstance3D = $ShadowMesh
+var ohiwa_start : bool = false
+var target : Player
+
+func _ready() -> void:
+	animation_player.play("RESET")
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is Player:
-		ohiwa.freeze = false
-		ohiwa.forward = forward
+		animation_player.play("Ohiwa Go")	
+		target = body
+
 
 func _physics_process(delta: float) -> void:
-	if !ohiwa.freeze:
-		shadow_mesh.position = ohiwa.position - Vector3.UP * 10
+	if ohiwa_start and target:
+		var goto_position = Vector3(global_position.x, target.global_position.y * 5, target.global_position.z)
+		ohiwa.global_position = ohiwa.global_position.lerp(goto_position, 0.003)
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	match anim_name:
+		"Ohiwa Go":
+			ohiwa_start = true
+
+
+func _on_area_3d_body_entered(body:Node3D) -> void:
+	if body is Player:
+		body.die()
+		target = null
+		ohiwa.position = Vector3.ZERO
