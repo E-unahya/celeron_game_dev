0b374034e48b79a02212b7099a807ceb85f4795f
e-unahya <65tougames@gmail.com>
2026-07-22 23:04:37 +0900

    大岩ステージを作成し、ステージセレクトの際に名前が出てくるように変更

--- 3DModel/Ultimate Platformer Pack - Dec 2021/Level and Mechanics/scene/door.gd
+++ 3DModel/Ultimate Platformer Pack - Dec 2021/Level and Mechanics/scene/door.gd
@@ -2,17 +2,23 @@ extends Area3D
 
 @onready var door: MeshInstance3D = $Door
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var stage_name_label: Label = $Control/StageNameLabel
 
 @export_file("*.tscn") var to_stage
+@export var stage_name : String = "Stage Name."
 
 func _ready() -> void:
 	door.rotation = Vector3.ZERO
+	stage_name_label.text = stage_name
+	stage_name_label.hide()
 
 func _on_body_entered(body: Node3D) -> void:
 	animation_player.play("Door")
+	stage_name_label.show()
 
 func _on_body_exited(body: Node3D) -> void:
 	animation_player.play_backwards("Door")
+	stage_name_label.hide()
 
 func _on_goto_stage_body_entered(body: Node3D) -> void:
 	Global.go_to_stage(to_stage)
--- 3DModel/check_point.gd
+++ 3DModel/check_point.gd
@@ -2,16 +2,25 @@ extends Area3D
 
 @onready var check_point_mesh: MeshInstance3D = $CheckPointMesh
 
+var is_activated := false
+
 func _ready() -> void:
 	check_point_mesh.position = Vector3.ZERO
-	check_point_mesh.rotation = Vector3.ZERO
+	check_point_mesh.rotation_degrees = Vector3.ZERO
 	check_point_mesh.hide()
 
 func _on_body_entered(body: Node3D) -> void:
+	if is_activated or not body is Player:
+		return
+
+	is_activated = true
+	monitoring = false
+	body.check_point = global_position
+
 	var tween = get_tree().create_tween()
 	check_point_mesh.show()
 	tween.set_ease(Tween.EASE_IN_OUT)
 	tween.set_parallel(true)
-	tween.tween_property(check_point_mesh, "position", Vector3(0, 1.5, 0), 1.0)
-	tween.tween_property(check_point_mesh, "rotation", Vector3(0, 3600, 0), 1.0)
-	body.check_point = global_position
+	tween.tween_property(check_point_mesh, "position", Vector3(0, 1.5, 0), 0.6)
+	tween.tween_property(check_point_mesh, "rotation_degrees", Vector3(0, 360, 0), 0.6)
+
